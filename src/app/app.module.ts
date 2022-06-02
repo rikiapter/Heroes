a28929f95267636c74e6e526d5a52fe24c5acdd8
ריקי אפטר <riki_apter@malam.com>
2022-06-02 09:57:40 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './pages/card/card.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
